5060b3643f3ed7a7403de00b8077e84a4115395d
Russ Penska <russpenska@mt-mbp-l4c961tvv6.home>
2024-07-04 09:41:01 +0100

    rename terraform resources to be ecaas specific
    Co-authored-by: Korinna Pinakoulaki <kpinakula@users.noreply.github.com>

--- api/main.tf
+++ api/main.tf
@@ -16,38 +16,20 @@ provider "aws" {
   shared_credentials_files = ["~/.aws/credentials"]
 }
 
-resource "aws_api_gateway_rest_api" "MyDemoAPI" {
-  name = "MyDemoAPI"
-  description = "This is my API for demonstration purposes."
+# Set up API Gateway
+resource "aws_api_gateway_rest_api" "ECaaSAPI" {
+  name = "ECaas API"
+  description = "API for ECaaS (Energy Calculation as a Service)."
 
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "MyDemoResource" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
-  path_part   = "mydemoresource"
-}
-
-resource "aws_api_gateway_stage" "api_deployment_stage" {
-  deployment_id = aws_api_gateway_deployment.api_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  stage_name    = "api_deployment"
-}
-
-resource "aws_api_gateway_method" "MyDemoMethod" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id   = aws_api_gateway_resource.MyDemoResource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "MyDemoIntegration" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+resource "aws_api_gateway_integration" "GatewayIntegration" {
+  rest_api_id = aws_api_gateway_rest_api.ECaaSAPI.id
+  resource_id = aws_api_gateway_resource.ApiResource.id
+  http_method = aws_api_gateway_method.GetApiMethod.http_method
   type        = "MOCK"
   request_templates = {
     "application/json" = jsonencode({
@@ -56,18 +38,25 @@ resource "aws_api_gateway_integration" "MyDemoIntegration" {
   }
 }
 
-resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
-  status_code = "200"
+# Set up /api method
+resource "aws_api_gateway_resource" "ApiResource" {
+  rest_api_id = aws_api_gateway_rest_api.ECaaSAPI.id
+  parent_id   = aws_api_gateway_rest_api.ECaaSAPI.root_resource_id
+  path_part   = "api"
 }
 
-resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+resource "aws_api_gateway_method" "GetApiMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.ECaaSAPI.id
+  resource_id   = aws_api_gateway_resource.ApiResource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration_response" "GetApiIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.ECaaSAPI.id
+  resource_id = aws_api_gateway_resource.ApiResource.id
+  http_method = aws_api_gateway_method.GetApiMethod.http_method
+  status_code = aws_api_gateway_method_response.GetApiMethodResponse.status_code
   response_templates = {
     "application/json" = jsonencode(
       {
@@ -78,21 +67,35 @@ resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
   }
 }
 
-resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+resource "aws_api_gateway_method_response" "GetApiMethodResponse" {
+  rest_api_id = aws_api_gateway_rest_api.ECaaSAPI.id
+  resource_id = aws_api_gateway_resource.ApiResource.id
+  http_method = aws_api_gateway_method.GetApiMethod.http_method
+  status_code = "200"
+}
+
+# Set up deployment
+resource "aws_api_gateway_deployment" "Deployment" {
+  rest_api_id = aws_api_gateway_rest_api.ECaaSAPI.id
 
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_rest_api.MyDemoAPI.id,
-      aws_api_gateway_rest_api.MyDemoAPI.description,
-      aws_api_gateway_resource.MyDemoResource.id,
-      aws_api_gateway_method.MyDemoMethod.id,
-      aws_api_gateway_integration.MyDemoIntegration.id,
-      aws_api_gateway_integration_response.MyDemoIntegrationResponse.response_templates
+      aws_api_gateway_rest_api.ECaaSAPI.id,
+      aws_api_gateway_rest_api.ECaaSAPI.description,
+      aws_api_gateway_resource.ApiResource.id,
+      aws_api_gateway_method.GetApiMethod.id,
+      aws_api_gateway_integration.GatewayIntegration.id,
+      aws_api_gateway_integration_response.GetApiIntegrationResponse.response_templates
     ]))
   }
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_api_gateway_stage" "DeploymentStage" {
+  deployment_id = aws_api_gateway_deployment.Deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.ECaaSAPI.id
+  stage_name    = "Deployment"  
 }
