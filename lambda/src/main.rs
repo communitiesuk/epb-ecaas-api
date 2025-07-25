e92499d9570856b2cbc7114e8c1d909bda4313d8
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-07-25 14:24:09 +0100

    emit original error in case of invalid request so correct message prefix in response

--- lambda/src/main.rs
+++ lambda/src/main.rs
@@ -44,9 +44,9 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let input = match resolve_products(input) {
         Ok(input) => input,
         Err(e) => {
-            return if let ResolvePcdbProductsError::InvalidRequest(validation_error) = e {
+            return if let ResolvePcdbProductsError::InvalidRequest(_) = e {
                 // don't wrap in the resolve product error because this is just down to the request being invalid
-                error_422(validation_error, aws_request_id)
+                error_422(e, aws_request_id)
             } else {
                 error_422(ResolveProductError(e.to_string()), aws_request_id)
             };
